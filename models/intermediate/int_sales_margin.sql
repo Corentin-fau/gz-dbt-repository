c70e3662749dcf51de4ba1ad4d1764e7733260cf
fau.corentin@gmail.com <fau.corentin@gmail.com>
2024-10-29 14:24:21 +0000

    ok

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,6 +2,6 @@ SELECT
     *
     , ROUND ((quantity * purchase_price),2) AS purchase_cost
     , ROUND ((revenue- (quantity * purchase_price)),2) AS margin
-FROM {{ref}"stg_gz_raw_data__product"}
-JOIN {{ref}"stg_gz_raw_data__sales"}
+FROM {{ref("stg_gz_raw_data__product")}}
+JOIN {{ref("stg_gz_raw_data__sales")}}
     USING (products_id)
